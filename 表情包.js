5146d94301ddf7a0840686da865e1e17dac7a708
one <39196952+shellingfordly@users.noreply.github.com>
2019-05-31 18:57:13 +0800

    Add files via upload

---
+++ 表情包.js
@@ -0,0 +1,128 @@
+// 原脚本https://github.com/CodedArtist/JSBox/tree/master/斗图
+$keyboard.barHidden = true;
+const imgCount = 12;  // 可选值 4、8、12、16
+if($app.env!=$env.keyboard){
+  $ui.alert("本脚本只能运行在键盘扩展中");
+  $app.close();
+}
+
+var query = '';
+var start = 0;
+
+$ui.render({
+  props: {
+    navBarHidden: 1,
+    statusBarStyle: 1
+  },
+  views: [{
+    type: "matrix",
+    props: {
+      //底部返回退出按键
+      footer: {
+        views: [
+          {
+              type: "button",
+              props: {
+                title: "退出",
+                titleColor: $color("#111"),
+                bgcolor: $color("#FFF"),
+              },
+              layout: function(make) {
+                make.right.top.bottom.inset(3)
+                make.size.equalTo(155)
+              },
+              events: {
+                tapped: function(sender) {
+                  $keyboard.next()
+                }
+              }
+            }, 
+            {
+              type: "button",
+              props: {
+                title: "返回",
+                titleColor: $color("#111"),
+                bgcolor: $color("#FFF"),
+              },
+              layout: function(make) {
+                make.left.top.bottom.inset(3)
+                make.size.equalTo(155)
+              },
+              events: {
+                tapped: function(sender) {
+                  $app.close()
+                }
+              }
+            },
+          ]
+      },
+      //其他属性
+      id: "imgs",
+      square: true,
+      columns: 4,
+      spacing: 1,
+      template: [{
+        type: "image",
+        layout: (make) => make.top.bottom.right.left.inset(0)
+      }],
+    },
+    layout: (make) => make.top.bottom.left.right.inset(0),
+    events: {
+      didSelect: (sender, indexPath, data) => {
+        let t = setInterval(() => {
+          $keyboard.moveCursor(1);
+          if ($keyboard.hasText) $keyboard.delete()
+          else clearInterval(t)
+        }, 10);
+        $device.taptic(0);
+        $('spinner').loading = true;
+        $http.get(data.image.src,)
+          .then(rsp => {
+            // 选择其中一个，注释另外两个。
+            $share.wechat(rsp.rawData); // 分享至 微信
+            // $share.qq(rsp.rawData);  // 分享至 QQ
+            // $share.tim(rsp.rawData); // 分享至 TIM
+            
+            $keyboard.next();
+          });
+      },
+      didLongPress: () => {
+        $device.taptic(0);
+        $('spinner').loading = true;
+        search();
+      },
+      ready: () => {
+        // 必须要有此延迟
+        $delay(0.1, () => {
+          if($keyboard.hasText){
+            $keyboard.getAllText( (text) => {
+              query = text + '表情'; 
+              search();
+            })
+          }
+          else{
+            $keyboard.next()
+          }
+        });
+      }
+    }
+  }, {
+    type: "spinner",
+    props: {
+      loading: true
+    },
+    layout: (make, view) => make.center.equalTo(view.super)
+    } ],
+});
+
+function search() {
+  $http.get(`https://pic.sogou.com/pics/json.jsp?query=${$text.URLEncode(query)}&st=5&start=${start}&xml_len=${imgCount}`)
+    .then(rsp => {
+      start+=imgCount;
+      $('spinner').loading = false;
+      let items = rsp.data.items;
+      let data = [];
+      for (let i in items) data.push({ image: { src: items[i].picUrl } });
+      $('imgs').data = data;
+    })
+}
